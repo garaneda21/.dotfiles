b483859b342d9caba748fbe5fb8b3e72aca58f65
garaneda21 <garaneda@utem.cl>
2025-04-10 19:39:34 -0400

    xremap

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "lervag/vimtex",
-    lazy = false,
-    config = function ()
-        -- vim.g.vimtex_view_method = "zathura"
-        vim.g.vimtex_view_general_viewer = "okular"
-        vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
-    end
-}
