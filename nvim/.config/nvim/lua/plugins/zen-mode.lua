672f3e760c6fcdd085e705cbf673a6a09ac18cfa
garaneda21 <garaneda@utem.cl>
2025-07-04 18:11:57 -0400

    atajo para zen-mode

--- nvim/.config/nvim/lua/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -1,19 +1,19 @@
 return {
 	"folke/zen-mode.nvim",
-	config = function()
-		vim.keymap.set("n", "<leader>z", ":ZenMode<CR>", { desc = "Toogle [z]enMode", silent = true })
-	end,
 	opts = {
 		window = {
 			backdrop = 1,
 			width = 120, -- width of the Zen window
-			height = 0.90, -- height of the Zen window
+			height = 0.95, -- height of the Zen window
 			options = {
 				number = false, -- disable number column
 				relativenumber = false, -- disable relative numbers
 				foldcolumn = "0", -- disable fold column
 			},
 		},
+	},
+	keys = {
+		{ "<leader>z", ":ZenMode<CR>", desc = "Toogle [z]enMode", silent = true },
 	},
 	-- callback where you can add custom code when the Zen window opens
 	on_open = function(win) end,
