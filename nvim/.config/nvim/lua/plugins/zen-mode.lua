a2fa81f1b899607c1a355ed3181dd17cd46e46b4
garaneda21 <garaneda@utem.cl>
2025-07-06 18:31:35 -0400

    config parar zen-mode

--- nvim/.config/nvim/lua/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -11,12 +11,19 @@ return {
 				foldcolumn = "0", -- disable fold column
 			},
 		},
+		-- callback where you can add custom code when the Zen window opens
+		on_open = function()
+			vim.o.linebreak = true
+			vim.o.spell = true
+			vim.o.spelllang = "en_us,es"
+		end,
+		-- callback where you can add custom code when the Zen window closes
+		on_close = function()
+			vim.o.linebreak = false
+			vim.o.spell = false
+		end,
 	},
 	keys = {
 		{ "<leader>z", ":ZenMode<CR>", desc = "Toogle [z]enMode", silent = true },
 	},
-	-- callback where you can add custom code when the Zen window opens
-	on_open = function(win) end,
-	-- callback where you can add custom code when the Zen window closes
-	on_close = function() end,
 }
